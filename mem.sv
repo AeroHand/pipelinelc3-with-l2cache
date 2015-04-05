4cc7b8f9f3fab143ac462a85fe4ecc8a75525882
Brandon Holland <beholla2@illinois.edu>
2015-04-05 11:10:28 -0500

    Updates mem.sv

--- mem.sv
+++ mem.sv
@@ -2,44 +2,30 @@ import lc3b_types::*;
 
 module mem
 (
-	input load_cc,
-	input lc3b_word ex_mem_pc_out, //mem_npc
-	input lc3b_word ex_mem_alu_out,
-	input lc3b_reg mem_wb_dest_out,
-	input lc3b_word wb_regfilemux_out,
-	input lc3b_control_word ctrl,
-
+	input lc3b_word mem_address_out;
+	input lc3b_eleven mem_cs_out;
+	input lc3b_word mem_npc_out;
+	input lc3b_nzp mem_cc_out;
+	input lc3b_word mem_aluresult_out;
+	input lc3b_word mem_ir_out;
+	input lc3b_nzp mem_drid_out;
 	
-	output lc3b_word sr_address_in,
-	output lc3b_word sr_data_in,
-	output lc3b_four sr_cs_in,
-	output lc3b_word sr_npc_in,
-	output lc3b_word sr_ir_in,
-	output lc3b_nzp sr_drid_in,
-	output lc3b_word sr_aluresult_in,
-	output logic branch_enable
+	output lc3b_word sr_address_in;
+	output lc3b_word sr_data_in;
+	output lc3b_four sr_cs_in;
+	output lc3b_word sr_npc_in;
+	output lc3b_word sr_aluresult_in;
+	output lc3b_word sr_ir_in;
+	output lc3b_nzp sr_drid_in;
 );
 
-assign sr_cs_in = //[10:7]mem_cs_out;
+// Transfer over all necessary signals
+assign sr_address_in = mem_address_out;
+assign sr_cs_in = [10:7]mem_cs_out;
 assign sr_aluresult_in = ex_mem_alu_out;
-assign sr_npc_in = ex_mem_pc_out;
+assign sr_npc_in = mem_npc_out;
+assign sr_aluresult_in = mem_aluresult_out;
+assign sr_ir_in = mem_ir_out;
+assign sr_drid_in = mem_drid_out;
 
-lc3b_nzp gencc_out;
-lc3b_reg cc_out;
-
-
-mux2 marmux
-(
-	.sel(ctrl.marmux_sel),
-	.a(ex_mem_alu_out),
-	.b(ex_mem_pc_out),
-	.f(marmux_out)
-);
-
-mux2 mdrmux
-(
-	.sel(ctrl.mdrmux_sel),
-	.a(ex_mem_alu_out),
-	.b(ex_mem_pc_out),
-	.f(mdrmux_out)
-);
+endmodule: mem
